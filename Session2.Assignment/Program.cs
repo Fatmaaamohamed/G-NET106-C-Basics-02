e58c23a59f36cf4db974c815ae34a5b9a0776c9e
Fatma_Mohamed <fm5252498@gmail.com>
2026-08-26 00:55:24 +0300

    Question12

--- Session2.Assignment/Program.cs
+++ Session2.Assignment/Program.cs
@@ -124,6 +124,16 @@ namespace Session2.Assignment
             ///Console.WriteLine(book?.Title);
 
             #endregion
+
+            #region Question12
+            /*Using title from the previous question, use ?? to print "Untitled" if title is null. Then use ??=
+            to assign title the value "Untitled" only if it's still null.*/
+           /// Book? book = null;
+           /// string? title = book?.Title;
+            ///Console.WriteLine(title ?? "Untitled");
+          ///  title ??= "Untitled";
+           /// Console.WriteLine($"title Value: {title}");
+            #endregion region 
         }
     }
 }
